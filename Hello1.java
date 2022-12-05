a08c3952b0707382a493da1bccd88110531145d0
rishiyadav1923 <ihsir18675@gmail.com>
2022-12-05 14:15:51 +0530

    third Commit

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,17 @@
 // This is a simple Java program.
 // FileName : "HelloWorld.java".
+// ava is one of the most popular and widely used programming languages and platforms. Java is fast, reliable, and secure. Java is used in every nook and corner from desktop to web applications, scientific supercomputers to gaming consoles, cell phones to the Internet.
+
+// Java is easy to learn, and its syntax is simple and easy to understand. It is based on C++ (so easier for programmers who know C++).
+
+// The process of Java programming can be simplified in three steps: 
+
+// Create the program by typing it into a text editor and saving it to a file – HelloWorld.java.
+// Compile it by typing “javac HelloWorld.java” in the terminal window.
+// Execute (or run) it by typing “java HelloWorld” in the terminal window.
+// The below-given program is the most simple program of Java printing “Hello World” to the screen. Let us try to understand every bit of code step by step.
+
+
 
 public class Hello1
 {
